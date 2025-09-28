57de77d1d54383115cd5063ed8194ea447bc6d2a
gmamaladze <gmamaladze@gmail.com>
2025-09-28 18:03:07 +0200

    Version is geberated form file. Script not needed.

--- extract-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Extract version from GITHUB_REF (e.g., refs/tags/v1.2.3)
-if [[ "$GITHUB_REF" =~ refs/tags/v([0-9]+\.[0-9]+\.[0-9]+) ]]; then
-  VERSION="${BASH_REMATCH[1]}"
-else
-  VERSION="dev"
-fi
-
-echo "VERSION=$VERSION" > version.env
